411402e7895c568ce39b818144350cdd7872b304
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-09-27 18:48:35 +0200

    Initial Commit

---
+++ class2v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class2v2
+//
+//  Created by Spencer Johnson on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
