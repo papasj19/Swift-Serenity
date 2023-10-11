2687eb245312f01a047c6b56894985183fcbc955
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-10-11 14:14:32 +0200

    adding my work for AC2

---
+++ class2v2/SecondViewContViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewContViewController.swift
+//  class2v2
+//
+//  Created by Spencer Johnson on 9/27/23.
+//
+
+import UIKit
+
+class SecondViewContViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ class2v2/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  class2v2
+//
+//  Created by Spencer Johnson on 9/27/23.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
